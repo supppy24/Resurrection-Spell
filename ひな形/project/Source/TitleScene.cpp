11b1a7eb66d3ef7d9225dbba278faac8dea74da2
student <student>
2026-05-22 14:55:33 +0900

    基盤作り

---
+++ ひな形/project/Source/TitleScene.cpp
@@ -0,0 +1,29 @@
+#include "TitleScene.h"
+
+TitleScene::TitleScene()
+{
+}
+
+TitleScene::~TitleScene()
+{
+}
+
+void TitleScene::Update()
+{
+	if (CheckHitKey(KEY_INPUT_P)) {
+		SceneManager::ChangeScene("PLAY");
+	}
+	if (CheckHitKey(KEY_INPUT_ESCAPE)) {
+		SceneManager::Exit();
+	}
+}
+
+
+void TitleScene::Draw()
+{
+	extern const char* Version();
+	DrawString(0, 20, Version(), GetColor(255,255,255));
+	DrawString(0, 0, "TITLE SCENE", GetColor(255,255,255));
+	DrawFormatString(100, 100, GetColor(255,255,255), "%4.1f", 1.0f / Time::DeltaTime());
+	DrawString(100, 400, "Push [P]Key To Play", GetColor(255, 255, 255));
+}
