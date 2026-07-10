ff84d23c56a1e61c0616e8949a3f4a4d7ac282a5
sui <aaa>
2026-07-10 13:39:50 +0900

    スライム

---
+++ ひな形/project/Source/Slime.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+class Slime
+{
+private:
+	int HP;
+	int Attack;
+	int Skill;
+	int Slimage;
+public:
+	Slime();
+	~Slime();
+	void Update();
+	void Draw();
+
+};
+
