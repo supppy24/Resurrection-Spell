e271cf7f8aa61b85adf6bd682ea8b5ed14827e9b
sui <aaa>
2026-07-14 13:12:21 +0900

    バトルログの追加

--- ひな形/project/Source/PlayScene.h
+++ ひな形/project/Source/PlayScene.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 #include "../Library/SceneBase.h"
 
 #include "Player.h"
@@ -15,20 +18,37 @@ public:
     void Draw() override;
 
 private:
+
+    // プレイヤーと敵
     Player player;
     Enemy enemy;
 
+    // 敵の種類
     int enemyType;
+
+    // コマンド画像
     int kImage;
 
+    // 敵出現メッセージ
     bool newEnemyMessage;
     int messageTimer;
 
+    // ゲーム終了フラグ
     bool isGameOver;
+
+    // プレイヤーターンかどうか
     bool isPlayerTurn;
 
+    // キー入力
     bool oldKey1;
     bool oldKey2;
     bool oldKey3;
     bool oldKey4;
+
+    // バトルログ
+    // 最新6件を表示する
+    std::vector<std::string> battleLog;
+
+    // ログを追加する関数
+    void AddLog(const std::string& text);
 };
